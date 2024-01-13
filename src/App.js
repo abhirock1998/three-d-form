e8b8ad02ad6faedb4f65f7619dfebf3c00bcb8e5
Jack1998 <yaduvanshiabhi1998@gmail.com>
2024-01-13 22:49:23 +0530

    chore: added shadow on inpu or button focus

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { MdPerson } from "react-icons/md";
+import { FaLock } from "react-icons/fa6";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app_form_container">
+        <div className="app_form_account_icon">
+          <MdPerson size={80} color="#90A4AE" />
+        </div>
+        <div className="form-icon-half" />
+        <h1>Welcome Back</h1>
+        <div className="app_form_input_container">
+          <div className="app_form_input_icon">
+            <MdPerson size={20} color="#90A4AE" />
+          </div>
+          <input type="email" placeholder="Email" />
+        </div>
+        <div className="app_form_input_container">
+          <div className="app_form_input_icon">
+            <FaLock size={20} color="#90A4AE" />
+          </div>
+          <input type="password" placeholder="Password" />
+        </div>
+        <input className="login-button" type="submit" value="Submit" />
+        <p>New here? Create account</p>
+        <p>Forgot your password?</p>
+      </div>
     </div>
   );
 }
